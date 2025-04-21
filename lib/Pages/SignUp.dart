a0d293953cc9190b5a20731825d6a81ce11231f2
Mawadah <Ataraxiamawa@gmail.com>
2025-04-21 09:44:40 +0300

    Fix: Sign Up Page components + colors

--- lib/Pages/SignUp.dart
+++ lib/Pages/SignUp.dart
@@ -20,33 +20,70 @@ class SignUpPage extends StatelessWidget {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  SizedBox(height: 80),
+
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Color(0xFF05262F),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+              Container(
                   // Email
-                  TextField(
+                  width: 290,
+                  child: TextField(
                     decoration: InputDecoration(
                       hintText: 'Email',
                       filled: true,
-                      fillColor: Color(0xFF9E9E9EB3),
+                      fillColor: Color(0xFFD6D6D6),
                     ),
                   ),
+              ),
+
                   SizedBox(height: 20),
+
+              Container(
                   // Phone
-                  TextField(
+                  width: 290,
+                  child: TextField(
                     decoration: InputDecoration(
                       hintText: 'Phone',
                       filled: true,
-                      fillColor: Color(0xFF9E9E9EB3),
+                      fillColor: Color(0xFFD6D6D6),
                     ),
                   ),
+              ),
+
                   SizedBox(height: 20),
+
+              Container(
                   // Password
-                  TextField(
+                  width: 290,
+                  child: TextField(
                     decoration: InputDecoration(
                       hintText: 'Password',
                       filled: true,
-                      fillColor: Color(0xFF9E9E9EB3),
+                      fillColor: Color(0xFFD6D6D6),
                     ),
                   ),
+              ),
                   SizedBox(height: 20),
+
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF05262F),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: Text('Submit'),
+                  )
                 ],
               ),
             ),
