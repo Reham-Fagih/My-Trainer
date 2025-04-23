45067b3ba089ee41d21a37606d853661124cc485
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2025-04-24 00:44:29 +0300

    Fix:removed comment

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -33,7 +33,6 @@ router.post("/login", async (req, res) => {
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(401).json({ message: "Invalid credentials" });
 
-    // If login is successful, you can return a success message (no JWT for now)
     res.status(200).json({ message: "Login successful" });
   } catch (err) {
     res.status(500).json({ message: "Error logging in" });
