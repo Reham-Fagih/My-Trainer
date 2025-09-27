b158fd623394362ebf843a731c96a3b227af0c56
Eithar <eithar.012@gmail.com>
2025-09-27 13:16:17 +0300

    Modify home page

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -7,6 +7,157 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
+
+      // // nav bar
+      // bottomNavigationBar: BottomAppBar(
+      //   color: Colors.transparent,
+      //   elevation: 0,
+      //   child: Container(
+      //     color: Colors.transparent,
+      //     height: 60.0,
+      //     padding: EdgeInsets.symmetric(horizontal: 30),
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //       children: <Widget>[
+      //         IconButton(
+      //           icon: Image.asset('assets/images/Profileicon.png'),
+      //           onPressed: () {
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => ProfilePage()),
+      //             );
+      //           },
+      //         ),
+      //         IconButton(
+      //           icon: Image.asset('assets/images/Homeicon.png'),
+      //           onPressed: () {
+      //             // Already in home
+      //           },
+      //         ),
+      //         IconButton(
+      //           icon: Image.asset('assets/images/Cameraicon.png'),
+      //           onPressed: () {
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(
+      //                 builder: (context) => UploadScreen(
+      //                   baseUrl: 'https://api.com', //temp
+      //                   userId: 'temp',
+      //                 ),
+      //               ),
+      //             );
+      //           },
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      bottomNavigationBar: Container(
+        height: 90,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/FooterBackground.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.person, color: Colors.white, size: 45),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.home, color: Colors.white, size: 45),
+              onPressed: () {
+                // Already in home
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.camera_alt_rounded,
+                  color: Colors.white, size: 40),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => UploadScreen(
+                      baseUrl: 'https://api.com', // temp
+                      userId: 'temp',             // temp
+                    ),
+                  ),
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.logout, color: Colors.white, size: 40),
+              onPressed: () async {
+                final confirm = await showDialog<bool>(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Confirm Logout"),
+                      content: const Text("Are you sure you want to logout?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, false),
+                          child: const Text("Cancel"),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, true),
+                          child: const Text("Logout"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+
+                if (confirm == true) {
+                  final prefs = await SharedPreferences.getInstance();
+                  await prefs.remove('authToken');
+                  Navigator.pushReplacementNamed(context, "/welcome");
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+
+  Widget buildMenuItem(BuildContext context, String label, String iconPath, Widget destinationPage) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => destinationPage),
+        );
+      },
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: AssetImage(iconPath),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
     );
   }
 }
